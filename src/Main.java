a5932a3502efafea90f394e03d4da7c10a6216bc
Eyadzz <eyadyoussef1336@gmail.com>
2021-04-26 05:21:59 +0200

    Updated Main

--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,59 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
+
         System.out.print("Enter number of cylinders I/O requests: ");
-        int n = in.nextInt();
+        int number = input.nextInt();
+
         System.out.print("Enter the number of requests: ");
         ArrayList<Integer> requests = new ArrayList<Integer>();
-        for (int i=0 ; i<n ; i++){
-            int x = in.nextInt();
+
+        for (int i=0 ; i<number ; i++){
+            int x = input.nextInt();
             requests.add(x);
         }
+
         System.out.print("Enter Initial head start cylinder: ");
-        int head = in.nextInt();
-        FCFS obj = new FCFS();
-        obj.exec(head, requests);
-        /*SSTF obj = new SSTF();
-        obj.exec(head, requests);*/
-        /*System.out.println("Enter the number of the algorithm which algorithm you want to apply?");
+        int head = input.nextInt();
+
         System.out.println("1- FCFS");
         System.out.println("2- SSTF");
         System.out.println("3- SCAN");
         System.out.println("4- C-SCAN");
         System.out.println("5- LOOK");
         System.out.println("6- C-LOOK");
-        int choice = in.nextInt();
-        if(choice==1) //fcfs
-            algo = new SSTF();
-         else if(choice == 2) {
-            algo = new SSTF();
+
+        int choice = input.nextInt();
+        if(choice==1)
+        {
+            FCFS fcfs=new FCFS();
+            fcfs.exec(head,requests);
+        }
+        if(choice==2)
+        {
+            SSTF sstf = new SSTF();
+            sstf.exec(head,requests);
+        }
+        if(choice==3)
+        {
+            System.out.print("Enter direction");
+            String direction = input.nextLine();
+            SCAN scan = new SCAN();
+            scan.execute(head,requests,direction);
+        }
+        if(choice==4)
+        {
 
         }
-        algo.exec(head,requests);*/
+        if(choice==5)
+        {
+
+        }
+        if(choice==6)
+        {
+
+        }
     }
 }
